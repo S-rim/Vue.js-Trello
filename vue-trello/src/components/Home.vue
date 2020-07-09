a7b0d2c2fc9872688f3f68bf27b68dc96374c794
Srim <deamoon0801@naver.com>
2020-07-09 12:55:28 +0900

    컴포넌트 기본 틀 생성

---
+++ vue-trello/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        home
+        <div>
+            Board List :
+            <ul>
+                <li>
+                    <router-link to="/b/1">Board1</router-link>
+                </li>
+                <li>
+                    <router-link to="/b/2">Board2</router-link>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
