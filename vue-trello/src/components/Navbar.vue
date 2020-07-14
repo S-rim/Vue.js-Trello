4660b2734c4d96b451a2ad52b0762e48d67a0fe8
Srim <deamoon0801@naver.com>
2020-07-14 16:20:29 +0900

    Navbar 컴포넌트 구현

--- vue-trello/src/components/Navbar.vue
+++ vue-trello/src/components/Navbar.vue
@@ -1,16 +1,93 @@
 <template>
     <div>
-        <router-link to="/">Home</router-link>
-        <router-link to="/login">login</router-link>
+        <nav class="header">
+            <div class="headerLogo">
+                <router-link to="/">Trello</router-link>
+            </div>
+            <div class="headerAuth">
+                <a href="" v-if="isAuth" @click.prevent="logout">Logout</a>
+                <router-link v-else to ="/login">Login</router-link>
+            </div>
+        </nav>
     </div>
 </template>
 
 <script>
+import { setAuthInHeader } from '../api'
 export default {
-    
+    data() {
+        return {
+        }
+    },
+    computed : {
+        isAuth() {
+            return !!localStorage.getItem('token')
+        }
+    },
+    methods : {
+        logout() {
+            delete localStorage.token
+            setAuthInHeader(null)
+            this.$router.push('/login')
+        }
+    }
 }
 </script>
 
 <style>
+.header 
+{
+  flex: none;
+  background-color: rgba(0,0,0,.15);
+  height: 32px;
+  padding: 4px;
+}
 
+.header a 
+{
+  display: block;
+  height: 30px;
+  line-height: 30px;
+  text-decoration: none;
+  color: rgba(255,255,255,.5);
+}
+
+.headerLogo
+{
+  position: absolute;
+  left: 50%;
+  top: 7px;
+  margin-left: -30px;
+  text-align: center;
+  font-weight: bolder;
+  font-size: 24px;
+}
+
+.headerLogo a:hover,
+.headerLogo a:focus 
+{
+  color: rgba(255,255,255,.9);
+}
+
+.headerAuth
+{
+  position: absolute;
+  right: 15px;
+  top: 5px;
+}
+
+.headerAuth a 
+{
+  border-radius: 2px;
+  padding: 0 10px;
+  background-color: rgba(255,255,255, .5);
+  color: white;
+  transition: all .3s;
+}
+
+.headerAuth a:hover,
+.headerAuth a:focus 
+{
+  background-color: rgba(255,255,255, .3);
+}
 </style>
