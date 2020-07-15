3bde6e773cfbcdf97ccddc79332310a92b2c0a99
Srim <deamoon0801@naver.com>
2020-07-15 13:35:35 +0900

    AddBoard 컴포넌트 생성

---
+++ vue-trello/src/components/AddBoard.vue
@@ -0,0 +1,55 @@
+<template>
+    <Modal>
+        <div slot="header">
+            <h2>create new board
+                <a href="" class="modalDefaultButton"
+                    @click.prevent="close">&times;
+                </a>
+            </h2>
+        </div>
+        <div slot="body">
+            <form id="addBoardForm" @submit.prevent="addBoard">
+                <input class="form-control" type="text" v-model="input" ref="input">
+            </form>
+        </div>
+        <div slot="footer">
+            <button class="btn" :class="{'btn-success':valid}" type="submit" borm="addBoardForm" :disabled="!valid">
+                create Board
+            </button>
+        </div>
+    </Modal>
+</template>
+
+<script>
+import Modal from './Modal.vue'
+export default {
+    components : {Modal},
+    data() {
+        return {
+            input : '',
+            valid : false
+        }
+    },
+    watch : {
+        input(v) {
+            this.valid = v.trim().length > 0
+        }
+    },
+    mounted() {
+        this.$refs.input.focus();
+    },
+    methods : {
+        close() {
+            this.$emit('close');
+        },
+        addBoard() {
+            this.$emit('close');
+            this.$emit('submit', this.input);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
