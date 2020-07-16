56b10d98c25ae0beee0c5f142c1922d163da93b9
Srim <deamoon0801@naver.com>
2020-07-16 14:15:56 +0900

    Vuex 스토어 개선

--- vue-trello/src/router/index.js
+++ vue-trello/src/router/index.js
@@ -5,15 +5,22 @@ import Login from '../components/Login.vue'
 import NotFound from '../components/NotFound.vue'
 import Board from '../components/Board.vue'
 import Card from '../components/Card.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
+
+const requireAuth = (to, from, next) => {
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
+  store.getters.isAuth ? next() : next(loginPath)
+}
 
 const router = new VueRouter({
   mode : 'history',
   routes : [
     {
       path : '/',
-      component : Home
+      component : Home,
+      // beforeEnter : requireAuth()
     },
     {
       path : '/login',
@@ -23,9 +30,12 @@ const router = new VueRouter({
         // 콜론을 넣어주면 bid라는 변수로 받을 수 있음.
         path : '/b/:bid',
         component : Board, 
+        // beforeEnter : requireAuth(),
         children : [
             {
-                path : 'c/:cid', component : Card
+                path : 'c/:cid', 
+                component : Card,
+                // beforeEnter : requireAuth()
             }
         ]
     },
